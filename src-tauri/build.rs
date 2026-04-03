174a68d66f35d1fb436cc7d95c995383de4138c7
leimu-hue <2627492139@qq.com>
2026-04-03 09:25:26 +0800

    fix: 添加相关配置, 修复 cargo test 报错

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -4,5 +4,30 @@ fn main() {
     println!("cargo:rerun-if-changed=icons/icon.icns");
     println!("cargo:rerun-if-changed=icons/icon.ico");
     println!("cargo:rerun-if-changed=tauri.conf.json");
-    tauri_build::build()
+
+    let mut attributes = tauri_build::Attributes::new();
+    #[cfg(windows)]
+    {
+        attributes = attributes
+            .windows_attributes(tauri_build::WindowsAttributes::new_without_app_manifest());
+        add_manifest();
+    }
+    tauri_build::try_build(attributes).unwrap();
+}
+
+#[cfg(windows)]
+fn add_manifest() {
+    static WINDOWS_MANIFEST_FILE: &str = "windows-app-manifest.xml";
+
+    let manifest = std::env::current_dir().unwrap().join(WINDOWS_MANIFEST_FILE);
+
+    println!("cargo:rerun-if-changed={}", manifest.display());
+    // Embed the Windows application manifest file.
+    println!("cargo:rustc-link-arg=/MANIFEST:EMBED");
+    println!(
+        "cargo:rustc-link-arg=/MANIFESTINPUT:{}",
+        manifest.to_str().unwrap()
+    );
+    // Turn linker warnings into errors.
+    println!("cargo:rustc-link-arg=/WX");
 }
